7c5306686bc3d6f30c91e9e39dcf9f8d957b8441
pryrotech <colbypry@gmail.com>
2024-09-07 21:05:16 -0230

    more curtain menu stuff

--- pts/curtain-menu.js
+++ pts/curtain-menu.js
@@ -5,3 +5,11 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+const elementToHide = document.getElementsByClassName('topnav'); // Replace 'elementId' with the actual ID of your element
+
+  if (screen.width > 960) {
+      elementToHide.style.display = 'none';
+  } else {
+      elementToHide.style.display = 'block';
+  }
